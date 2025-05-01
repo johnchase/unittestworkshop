f625d632b75676421648ef64c9af78f4baea07ec
John Chase <johnhchase@protonmail.com>
2025-05-01 08:45:57 -0700

    Adds more tests

--- src/tests/test_fixtures.py
+++ src/tests/test_fixtures.py
@@ -1,27 +1,31 @@
 import pandas as pd
+import pytest
 
 from utw.fixtures import filter_counts
 
-DF_TEST = pd.DataFrame(
-    {
-        "name": ["s1", "s2", "s3", "s4", "s5"],
-        "counts": [3000, 175, 45, 500, 1500],
-    }
-)
+
+@pytest.fixture
+def df_test():
+    return pd.DataFrame(
+        {
+            "name": ["s1", "s2", "s3", "s4", "s5"],
+            "counts": [3000, 175, 45, 500, 1500],
+        }
+    )
 
 
-def test_filter_counts():
+def test_filter_counts(df_test):
     """Test the filter_counts function."""
 
-    filtered = filter_counts(DF_TEST, threshold=1000)
-    DF_TEST.drop(index=filtered.index, inplace=True)
-    assert len(filtered) == 2
-    assert all(filtered["counts"] >= 1000)
+    filtered = filter_counts(df_test, threshold=1000)
+    df_test.drop(index=filtered.index, inplace=True)
+    assert len(df_test) == 2
+    assert all(df_test["counts"] >= 1000)
 
 
-def test_filter_counts_1():
+def test_filter_counts_1(df_test):
     """Test the filter_counts function."""
 
-    filtered_df = filter_counts(DF_TEST, threshold=10)
+    filtered_df = filter_counts(df_test, threshold=10)
     assert len(filtered_df) == 5
     assert all(filtered_df["counts"] >= 10)
